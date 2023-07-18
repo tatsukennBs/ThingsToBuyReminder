7c6a282b964e2f6fdcef59b28e060c779a50af65
Tatsunori <tatsunori.palmeaves@gmail.com>
2023-07-18 19:28:32 +0900

    入力項目の単項目チェック追加

--- src/main/java/com/example/demo/app/ThingsToBuyForm.java
+++ src/main/java/com/example/demo/app/ThingsToBuyForm.java
@@ -2,14 +2,26 @@ package com.example.demo.app;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
 public class ThingsToBuyForm {
 	
+	@NotBlank (message = "品目名を入力してください")
+	@Size(max = 128, message = "品目名は128文字以内で入力してください")
 	private String itemName;
 	
+	@NotBlank (message = "カテゴリを入力してください")
+	@Size(max = 128, message = "カテゴリは128文字以内で入力してください")
 	private String category;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date purchaseDate;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date purchaseinterval;
 	
 	public ThingsToBuyForm() {
@@ -47,5 +59,4 @@ public class ThingsToBuyForm {
 	public void setPurchaseinterval(Date purchaseinterval) {
 		this.purchaseinterval = purchaseinterval;
 	}
-
 }
